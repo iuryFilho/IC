779b3208b93b54af5e48667d5295346160e857e5
Iury Filho <iuryfmmf@gmail.com>
2025-06-13 15:21:37 -0300

    muda formato do log

--- algorithm.py
+++ algorithm.py
@@ -4,6 +4,7 @@ from numpy.linalg import norm
 from typing import Callable
 from numpy.typing import NDArray
 from numbers import Number
+import time
 
 
 def find_dk(Bk: NDArray, fgxk: NDArray, sec: NDArray) -> NDArray:
@@ -43,6 +44,10 @@ def def_g(g_list):
     return lambda x: np.array([g(x) for g in g_list])
 
 
+def get_time() -> float:
+    return time.time() * 1e3
+
+
 def solver(
     x0: NDArray,
     x1: NDArray,
@@ -56,6 +61,8 @@ def solver(
     t_max: float = 2 / 3,
     eta: Callable[[Number], float] = _eta,
 ):
+    time_mult = 1e3
+
     g = def_g(g_list)
     fg = lambda x: f(x) + g(x)
     xk_1 = x0
@@ -66,19 +73,27 @@ def solver(
     dk = find_dk(Bk, fgxk, sec)
     a = 1
 
-    log: dict[str, list[NDArray]] = {"xk": [xk_1], "f+g": [fg(xk_1)]}
+    log: dict[str, list[NDArray]] = {"xk": [xk_1], "f+g": [fg(xk_1)], "time": [0.0]}
     log["xk"].append(xk)
     log["f+g"].append(fgxk)
+    log["time"].append(0.0)
     k = 1
+
+    time_start = get_time()
     while True:
         if norm(fg(xk + a * dk)) <= (1 + a * s * (th - 1)) * norm(fgxk) + eta(k):
             k += 1
             new_xk = xk + a * dk
             new_fgxk = fg(new_xk)
+            if norm(new_fgxk) < tol:
+                log["time"].append(get_time() - time_start)
+                log["xk"].append(new_xk)
+                log["f+g"].append(new_fgxk)
+                return new_xk, log
+            log["time"].append(get_time() - time_start)
             log["xk"].append(new_xk)
             log["f+g"].append(new_fgxk)
-            if norm(new_fgxk) < tol:
-                return new_xk, log
+            time_start = get_time()
             sk = new_xk - xk
             yk = new_fgxk - fgxk
             Bk = get_new_Bk(Bk, sk, yk)
@@ -92,8 +107,11 @@ def solver(
             a = uniform(a * t_min, a * t_max)
 
 
+number_format = lambda x: f"{x:.6f}" if x < 0 else f" {x:.6f}"
+
+
 def flog(log) -> str:
-    return np.array2string(log, suppress_small=True)
+    return np.array2string(log, formatter={"float_kind": number_format})
 
 
 def main():
@@ -105,9 +123,9 @@ def main():
         lambda x: x[0] + x[1] - 7 + abs(x[1]) / 9,
     ]
 
-    x0 = np.array([0, 0])
-    x1 = np.array([1, 1])
-    B0 = np.array([[1, 0], [0, 1]])
+    x0 = np.array([0.0, 0.0])
+    x1 = np.array([1.0, 1.0])
+    B0 = np.array([[1.0, 0.0], [0.0, 1.0]])
 
     x, log = solver(x0, x1, B0, f1, g1_list)
     print("x0:")
@@ -119,10 +137,15 @@ def main():
     print("Solution found:")
     print(x)
     print("Log:")
-    for i in range(len(log["xk"])):
-        print(
-            f"x{i} = {flog(log['xk'][i])}, (f+g){i} = {flog(log['f+g'][i])}, ||(f+g){i}|| = {flog(norm(log['f+g'][i]))}"
-        )
+    with open("log.txt", "w", encoding="utf-8") as f:
+        f.write("k, xk, ||(f+g)||, time (ms) \n")
+        for i in range(len(log["xk"])):
+            f.write(
+                f"{i}, {flog(log['xk'][i])}, {flog(norm(log['f+g'][i]))}, {log['time'][i]:.6f}\n"
+            )
+            print(
+                f"x{i:02} = {flog(log['xk'][i])}, ||(f+g){i:02}|| = {flog(norm(log['f+g'][i]))}, time = {log['time'][i]:.6f}"
+            )
 
 
 if __name__ == "__main__":
